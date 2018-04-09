2224f6711a7fbefb3021384de0ba505f1f82a686
trumblonie <38194232+trumblonie@users.noreply.github.com>
2018-04-08 20:03:49 -0400

    803

--- CallTrackingChange.js
+++ CallTrackingChange.js
@@ -1,22 +1,18 @@
-//JQuery Extension
-$.extend({
-  getUrlVars: function(){
+//JQuery Extension   http://jquery-howto.blogspot.com/2009/09/get-url-parameters-values-with-jquery.html
+function getUrlVars()
+{
     var vars = [], hash;
     var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
     for(var i = 0; i < hashes.length; i++)
     {
-      hash = hashes[i].split('=');
-      vars.push(hash[0]);
-      vars[hash[0]] = hash[1];
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
     }
     return vars;
-  },
-  getUrlVar: function(name){
-    return $.getUrlVars()[name];
-  }
-});
+}
 
-var ppcLoc = $.getUrlVar('ppc');
+var ppcLoc = $.getUrlVar()["ppc"];
 
 switch (ppcLoc){
 	Case 1:
