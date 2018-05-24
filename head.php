fd2013bba200eceeac5b780a0f5faf131c883624
trumblonie <38194232+trumblonie@users.noreply.github.com>
2018-05-23 22:50:44 -0400

    1050

---
+++ head.php
@@ -0,0 +1,41 @@
+<link rel="canonical" href="https://<?php echo $_SERVER['HTTP_HOST']; ?><?php echo parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH ); ?>" />
+<link href="/lib/css/min.stylesheet.css" rel="stylesheet" type="text/css" media="screen" />
+<meta name="viewport" content="initial-scale=1, maximum-scale=1" />
+<link href="/lib/img/global/favicon.ico" type="image/png" rel="icon" />
+<meta name="google-site-verification" content="iCuqbJnkesGgY5qwaHoPikpJZdbKbILpUe6vTrI9lXM" />
+<script type="text/javascript" src="/lib/js/jquery-3.2.1.min.js"></script>
+<!-- <script type="text/javascript" src="/lib/js/jquery-1.8.2.min.js"></script>-->
+<link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700" rel="stylesheet" />
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-83249946-1', 'auto');
+ga('send', 'pageview');
+
+</script>
+<script src="//load.sumome.com/" data-sumo-site-id="655fbd8fad487dd640d367b4bc560fd082c9b6d05c2e043b53bf53534f6edf6e" async="async"></script>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WV7J49Z');</script>
+<!-- End Google Tag Manager -->
+
+<!-- Lead Simple -->
+<script type='text/javascript'>
+  (function() {
+    var ls = document.createElement('script');
+    ls.type = 'text/javascript';
+    ls.async = true;
+    ls.src = '//api.leadsimple.com/sdk/5fbf2ed2f16c0a32be4e.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ls, s);
+  })();
+</script>
+<!-- End Lead Simple -->
